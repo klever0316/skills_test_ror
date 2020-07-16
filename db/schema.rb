dc32717a3edccd2f892bc34d83aecf26996ed741
Klever Sobrinho <kleverfortaleza@gmail.com>
2020-07-16 08:47:29 -0300

    Finish initial settings

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_162758) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
-    t.text "bio"
+    t.text "biography"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
